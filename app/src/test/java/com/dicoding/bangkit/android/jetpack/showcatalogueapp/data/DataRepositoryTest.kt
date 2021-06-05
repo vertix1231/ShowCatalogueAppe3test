6bd7df885d31dd2af32d023415e304c5f30f15ac
Irsyad Muhammad Andre <irsyad_vertix@apps.ipb.ac.id>
2021-06-05 15:00:56 +0700

    android dicoding submisi 3 jetpack test ke 13 (include testing)

--- app/src/test/java/com/dicoding/bangkit/android/jetpack/showcatalogueapp/data/DataRepositoryTest.kt
+++ app/src/test/java/com/dicoding/bangkit/android/jetpack/showcatalogueapp/data/DataRepositoryTest.kt
@@ -7,13 +7,11 @@ import com.dicoding.bangkit.android.jetpack.showcatalogueapp.db.ShowtaimentDao
 import com.dicoding.bangkit.android.jetpack.showcatalogueapp.db.ShowtaimentEntity
 import com.nhaarman.mockitokotlin2.verify
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
+import org.junit.*
 import org.junit.runner.RunWith
 import org.koin.core.context.GlobalContext
 import org.koin.test.KoinTest
@@ -23,10 +21,10 @@ import org.mockito.MockitoAnnotations
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
+
 @RunWith(RobolectricTestRunner::class)
 @Config(maxSdk = Build.VERSION_CODES.P, minSdk = Build.VERSION_CODES.P)
-class DataRepositoryTest:KoinTest {
-
+class DataRepositoryTest : KoinTest{
     private lateinit var repository: DataRepository
     private val dispatcher = TestCoroutineDispatcher()
 
@@ -58,4 +56,93 @@ class DataRepositoryTest:KoinTest {
         verify(dao).getFavoriteList("tv")
     }
 
+    @Test
+    fun getFilms() {
+        runBlocking {
+            val films = repository.getFilms()
+            Assert.assertNotNull(films)
+            val data = films.results[0].originalTitle
+            println(data)
+        }
+    }
+
+    @Test
+    fun getTvs() {
+        runBlocking {
+            val tvs = repository.getTvs()
+            Assert.assertNotNull(tvs)
+            val data = tvs.results[0].originalName
+            println(data)
+        }
+    }
+
+    @Test
+    fun getFilmDetail() {
+        runBlocking {
+            val films = repository.getFilms()
+            Assert.assertNotNull(films)
+            val data = films.results[0]
+            val filmId = data.id
+            val filmDetail = repository.getFilmDetail(filmId)
+            Assert.assertEquals(data.originalTitle, filmDetail.originalTitle)
+        }
+    }
+
+    @Test
+    fun getTvDetail() {
+        runBlocking {
+            val tvs = repository.getTvs()
+            Assert.assertNotNull(tvs)
+            val data = tvs.results[0]
+            val tvId = data.id
+            val tvDetail = repository.getTvDetail(tvId)
+            Assert.assertEquals(data.originalName, tvDetail.originalName)
+        }
+    }
+
+    @Test
+    fun getFilmRating() {
+        runBlocking {
+            val films = repository.getFilms()
+            Assert.assertNotNull(films)
+            val data = films.results[0]
+            val filmId = data.id
+            val rating = repository.getFilmRating(filmId)
+            Assert.assertNotNull(rating)
+            Assert.assertEquals(data.id, rating.id)
+        }
+    }
+
+    @Test
+    fun getTvRating() {
+        runBlocking {
+            val tvs = repository.getTvs()
+            Assert.assertNotNull(tvs)
+            val data = tvs.results[0]
+            val tvId = data.id
+            val rating = repository.getTvRating(tvId)
+            Assert.assertNotNull(rating)
+            Assert.assertEquals(rating.id, data.id)
+        }
+    }
+
+    @Test
+    fun testInsert() {
+        val artEntity =
+            ShowtaimentEntity(id = 1, title = "raisa", photo = "tasya", type = "tv", year = "2020")
+        runBlocking {
+            repository.insert(artEntity)
+            verify(dao).insert(artEntity)
+        }
+    }
+
+    @Test
+    fun testDelete() {
+        val artEntity =
+            ShowtaimentEntity(id = 1, title = "raisa", photo = "tasya", type = "tv", year = "2020")
+        runBlocking {
+            repository.delete(artEntity)
+            verify(dao).delete(artEntity)
+        }
+    }
 }
